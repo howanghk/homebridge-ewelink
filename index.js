c80e18d3376ef0c7316c5429c7b6341fae5d1153
David Hutchison <dhutchison86+github@googlemail.com>
2020-06-22 01:06:57 +0100

    Refactored WebSocketClient out into a seperate file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let nonce = require('nonce')();
 let crypto = require('crypto');
 
 let LanClient = require('./lib/sonoffLanModeApi');
+let WebSocketClient = require('./lib/webSocketClient');
 
 let wsc;
 let isSocketOpen = false;
@@ -2213,83 +2214,6 @@ eWeLink.prototype.getArguments = function () {
     args.romVersion = '11.1.2';
     args.appVersion = '3.5.3';
     return querystring.stringify(args);
-};
-
-/* WEB SOCKET STUFF */
-
-function WebSocketClient() {
-    this.number = 0; // Message number
-    this.autoReconnectInterval = 5 * 1000; // ms
-    this.pendingReconnect = false;
-}
-
-WebSocketClient.prototype.open = function (url) {
-    this.url = url;
-    this.instance = new WebSocket(this.url);
-    this.instance.on('open', () => {
-        this.onopen();
-    });
-
-    this.instance.on('message', (data, flags) => {
-        this.number++;
-        this.onmessage(data, flags, this.number);
-    });
-
-    this.instance.on('close', (e) => {
-        switch (e) {
-            case 1000: // CLOSE_NORMAL
-                // console.log("WebSocket: closed");
-                break;
-            default: // Abnormal closure
-                this.reconnect(e);
-                break;
-        }
-        this.onclose(e);
-    });
-    this.instance.on('error', (e) => {
-        switch (e.code) {
-            case 'ECONNREFUSED':
-                this.reconnect(e);
-                break;
-            default:
-                this.onerror(e);
-                break;
-        }
-    });
-};
-WebSocketClient.prototype.send = function (data, option) {
-    try {
-        this.instance.send(data, option);
-    } catch (e) {
-        this.instance.emit('error', e);
-    }
-};
-WebSocketClient.prototype.reconnect = function (e) {
-    // console.log(`WebSocketClient: retry in ${this.autoReconnectInterval}ms`, e);
-
-    if (this.pendingReconnect) return;
-    this.pendingReconnect = true;
-
-    this.instance.removeAllListeners();
-
-    let platform = this;
-    setTimeout(function () {
-        platform.pendingReconnect = false;
-        console.log("WebSocketClient: reconnecting...");
-        platform.open(platform.url);
-    }, this.autoReconnectInterval);
-};
-WebSocketClient.prototype.onopen = function (e) {
-    // console.log("WebSocketClient: open", arguments);
-};
-WebSocketClient.prototype.onmessage = function (data, flags, number) {
-    // console.log("WebSocketClient: message", arguments);
-};
-WebSocketClient.prototype.onerror = function (e) {
-    console.log("WebSocketClient: error", arguments);
-};
-WebSocketClient.prototype.onclose = function (e) {
-    // console.log("WebSocketClient: closed", arguments);
 };
 
 //////////////
